a449b64afc90d299b2b9e6ebe8022a8acc30d43c
hanjianqiao <hanjianqiao@gmail.com>
2017-11-05 11:20:17 +0800

    added:handle signal

---
+++ samples/ipc/signal/system_signal_handles.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+
+void sigint_handler(int sig)
+{
+    char str[32];
+    sprintf(str, "Get signal num: %d\n", sig);
+    write(0, str, strlen(str));
+}
+
+int main(void)
+{
+    void sigint_handler(int sig); /* prototype */
+    char s[200];
+    struct sigaction sa;
+
+    sa.sa_handler = sigint_handler;
+    sa.sa_flags = 0; // or SA_RESTART
+    sigemptyset(&sa.sa_mask);
+
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    printf("Enter a string:\n");
+
+    if (fgets(s, sizeof s, stdin) == NULL)
+        perror("fgets");
+    else 
+        printf("You entered: %s\n", s);
+
+    return 0;
+}
